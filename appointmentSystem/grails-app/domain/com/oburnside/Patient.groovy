0ac52f9bef32bac8dd219237e0efd5cc2cefd4ea
username <email_address>
2019-03-11 14:28:58 +0000

    Added contraints to Receptionist

--- appointmentSystem/grails-app/domain/com/oburnside/Patient.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Patient.groovy
@@ -11,5 +11,13 @@ Date dateRegistered
 int patientPhone
 
     static constraints = {
+
+patientName blank: false, nullable: false, maxSize: 25
+patientAddress blank: false, nullable: false, maxSize: 50
+patientResidence blank: false, nullable: false, maxSize: 25
+patientDOB blank: false, nullable: false
+patientID blank: false, nullable: false
+dateRegistered blank: false, nullable: false, maxSize: 25
+patientPhone blank: false, nullable: false, max: 11
     }
 }
--- appointmentSystem/grails-app/domain/com/oburnside/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Receptionist.groovy
@@ -9,5 +9,11 @@ String recepPassword
 int recepPhone
 
     static constraints = {
+
+recepName blank: false, nullable: false, maxSize: 25
+recepEmail blank: false, nullable: false, maxSize: 25, email: true
+recepUsername blank: false, nullable: false, maxSize: 10
+recepPassword blank: false, nullable: false, maxSize: 25, minSize: 6
+recepPhone blank: false, nullable: false, max: 11
     }
 }
