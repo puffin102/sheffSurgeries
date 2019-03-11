64d9b1dcd4dbd639761d5a3c60d7cdb6083c7eee
username <email_address>
2019-03-11 12:21:56 +0000

    Declared dataTypes in Receptionist

--- appointmentSystem/grails-app/domain/com/oburnside/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Receptionist.groovy
@@ -2,6 +2,12 @@ package com.oburnside
 
 class Receptionist {
 
+String recepName
+String recepEmail
+String recepUsernanme
+String recepPassword
+int recepPhone
+
     static constraints = {
     }
 }
