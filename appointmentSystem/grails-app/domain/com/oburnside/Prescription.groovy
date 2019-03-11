96f66ca7c61cbb901b9dcccbe67a8fa962723669
username <email_address>
2019-03-11 13:30:16 +0000

    Added constraints to Presecription

--- appointmentSystem/grails-app/domain/com/oburnside/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Prescription.groovy
@@ -10,5 +10,14 @@ Date dateIssued
 Boolean patientPaying
 
     static constraints = {
+
+pharmacyName blank: false, nullable: false, maxSize: 30
+prescripNumber blank: false, nullable: false, max: 10
+medicine blank: false, nullable: false, maxSize: 50
+totalCost blank: false, nullable: false
+dateIssued blank: false, nullable: false
+patientPaying blank: false, nullable: false
+
+
     }
 }
