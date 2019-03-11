25d18d23d82fa669b85f3a3819b2cfecb6c70e36
username <email_address>
2019-03-11 15:26:15 +0000

    Adding a controller for Appointment

---
+++ appointmentSystem/grails-app/controllers/appointmentsystem/AppointmentController.groovy
@@ -0,0 +1,6 @@
+package com.oburnside
+
+class AppointmentController {
+
+    def scaffold = Appointment
+}
---
+++ appointmentSystem/grails-app/controllers/appointmentsystem/NurseController.groovy
@@ -0,0 +1,6 @@
+package appointmentsystem
+
+class NurseController {
+
+    def index() { }
+}
