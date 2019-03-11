ba3ed1e84ad02851de1a8671b86b5f454c794b22
username <email_address>
2019-03-11 14:22:51 +0000

    Added contraints to Doctor

--- appointmentSystem/grails-app/domain/com/oburnside/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Doctor.groovy
@@ -12,5 +12,13 @@ int doctorPhone
 String bio
 
     static constraints = {
+
+fullName blank: false, nullable: false, maxSize: 25
+qualification blank: false, nullable: false, maxSize: 50
+position blank: false, nullable: false, maxSize: 20
+doctorEmail blank: false, nullable: false, maxSize: 25, email: true
+password blank: false, nullable: false, maxSize: 25, minSize: 6
+doctorOffice blank: false, nullable: false, maxSize: 10
+bio blank: false, nullable: false, maxSize: 100
     }
 }
