ca67b777ad45965e79f42d7af23dbb36bc9e1361
username <email_address>
2019-03-11 12:12:58 +0000

    Declared dataTypes in Surgery

--- appointmentSystem/grails-app/domain/com/oburnside/Surgery.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Surgery.groovy
@@ -2,6 +2,14 @@ package com.oburnside
 
 class Surgery {
 
+String name
+String address
+String postcode
+int telephone
+int numberOfPatients
+String description
+double openingTime
+
     static constraints = {
     }
 }
