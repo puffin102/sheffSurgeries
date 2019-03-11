615d5c3bb82fd1ff846bec18564d3d9304051152
username <email_address>
2019-03-11 12:16:32 +0000

    Declared dataTypes in Doctor

--- appointmentSystem/grails-app/domain/com/oburnside/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/oburnside/Doctor.groovy
@@ -2,6 +2,15 @@ package com.oburnside
 
 class Doctor {
 
+String fullName
+String qualification
+String position
+String doctorEmail
+String password
+String doctorOffice
+int doctorPhone
+String bio
+
     static constraints = {
     }
 }
